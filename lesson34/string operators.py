23d0562e777f730343cbabac7a0795dcb1915b30
ABI-0327 <purusothaman1980@gmail.com>
2025-04-26 20:09:16 +0530

    first commit

---
+++ lesson34/string operators.py
@@ -0,0 +1,14 @@
+first_name = "Abisathana"
+last_name = " Purusothaman"
+full_name = first_name+last_name
+example = "Haa"*6
+print("First Name :", first_name)
+print("Last Name :", last_name)
+print("Full Name :", full_name)
+print("String Multiplied 6 times  gives this result :", example)
+
+word = "Abisathana"
+print("Lenght of String :", len(word))
+print("First Letter of String :", word[0])
+print("Last Letter of String :", word[2])
+print("String Sliced :", word[0:3])
