23c2652458a1f1e394a0bcbfc94e87042eb13e64
ABI-0327 <purusothaman1980@gmail.com>
2025-05-12 20:30:32 +0530

    lesson46 2nd act

---
+++ lesson46/student2.py
@@ -0,0 +1,15 @@
+class student:
+    grade=9
+    name="Abisathana"
+    def introduction(self):
+       print("I am a student")
+    def details(self):
+       print("my name is " , self.name)
+       print("I study in grade", self.grade)
+obj=student()
+obj.introduction()
+obj.details()
+
+
+    
+
